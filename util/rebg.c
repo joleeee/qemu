0f91660a6804d3314e092f8636307cbab90fdad5
Jonas <jonas@jole.xyz>
2023-06-08 16:01:15 +0000

    fix default output

--- util/rebg.c
+++ util/rebg.c
@@ -4,12 +4,15 @@
 // good idea
 #include "qemu/rebg.h"
 
-FILE *rebg_fd = (FILE*)2; // stderr
+FILE *rebg_fd = NULL;
 
 void rebg_handle_filename(const char * arg) {
     rebg_fd = fopen(arg, "wb");
 }
 
 FILE *rebg_log_fd(void) {
+    if(rebg_fd == NULL) {
+        rebg_fd = stderr;
+    }
     return rebg_fd;
 }
